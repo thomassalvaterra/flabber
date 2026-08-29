afb749f8f5e1076caaa24644930350a20fc9e84d
Thomas <thomassalvaterra2001@gmail.com>
2026-08-29 13:31:01 +0200

    Refine welcome logo and wordmark

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
+import { Geist, Geist_Mono, Montserrat } from 'next/font/google';
 import './globals.css';
 
 const geistSans = Geist({
@@ -10,6 +10,12 @@ const geistSans = Geist({
 const geistMono = Geist_Mono({
   variable: '--font-geist-mono',
   subsets: ['latin'],
+});
+
+const flabberBrand = Montserrat({
+  variable: '--font-flabber-brand',
+  subsets: ['latin'],
+  weight: ['800', '900'],
 });
 
 export const metadata: Metadata = {
@@ -62,7 +68,7 @@ export default function RootLayout({
     <html lang="it">
       <head><script dangerouslySetInnerHTML={{ __html: welcomeBoot }} /></head>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${geistSans.variable} ${geistMono.variable} ${flabberBrand.variable} antialiased`}
       >
         {children}
       </body>
